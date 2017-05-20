e1bcf974d6fb1ac2096a7585fb2d9ee64171fed4
DuGuLeTian <lth1429081529@gmail.com>
2017-05-21 02:13:41 +0800

    fix

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,15 +5,21 @@
         .module('yikatongApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'toaster', 'Card'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, $state, toaster, Card) {
         var vm = this;
 
+        vm.currentName = $state.current.name;
+        vm.cards = [];
+        // console.log(vm.currentName);
+        vm.cardIsReclaimed = false;
+        vm.cardNumber = null;
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+        vm.searchCardNumber = searchCardNumber;
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
@@ -28,6 +34,31 @@
         }
         function register () {
             $state.go('register');
+        }
+        function searchCardNumber(cardNumber) {
+            // console.log(cardNumber);
+
+            if (vm.cardNumber === null) {
+                toaster.pop('error', '', '请输入卡号');
+            } else {
+                Card.query(function(result) {
+                    vm.cards = result;
+
+                });
+                for (var i = 0; i < vm.cards.length; i++) {
+                    if (parseInt(vm.cardNumber) === vm.cards[i].number) {
+                        toaster.pop('success', '', '您的一卡通已被系统回收');
+                        vm.cardIsReclaimed = true;
+                        break;
+                    }
+                }
+
+                if (vm.cardIsReclaimed === false) {
+                    toaster.pop('error', '', '您的一卡通未被系统回收');
+                }
+            }
+            vm.cardNumber = null;
+            vm.cardIsReclaimed = false;
         }
     }
 })();
