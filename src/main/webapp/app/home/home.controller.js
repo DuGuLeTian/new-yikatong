1a8048259dcfcaf0a50c614fa57e6fc1190cd2db
DuGuLeTian <lth1429081529@gmail.com>
2017-05-30 23:51:14 +0800

    add router to blog

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -7,58 +7,55 @@
 
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'toaster', 'Card'];
 
-    function HomeController ($scope, Principal, LoginService, $state, toaster, Card) {
+    function HomeController($scope, Principal, LoginService, $state, toaster, Card) {
         var vm = this;
 
         vm.currentName = $state.current.name;
-        vm.cards = [];
         // console.log(vm.currentName);
-        vm.cardIsReclaimed = false;
-        vm.cardNumber = null;
+        // vm.pageState = 'home';
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        vm.searchCardNumber = searchCardNumber;
-        $scope.$on('authenticationSuccess', function() {
+        vm.jumpFromHome = jumpFromHome;
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
-        function register () {
+
+        function register() {
             $state.go('register');
         }
-        function searchCardNumber(cardNumber) {
+
+        function jumpFromHome(pageState) {
             // console.log(cardNumber);
 
-            if (vm.cardNumber === null) {
-                toaster.pop('error', '', '请输入卡号');
-            } else {
-                Card.query(function(result) {
-                    vm.cards = result;
+            //这里想使用 （闭包函数？） state.go跳转 判断路由状态是否存在，如果不存在就、、、
+            //或者县跳转在判断，没有上面的方法好，但是这种实现需要学习。state.go如果跳转失败执行相关语句的方法
+            //仅仅是为了深入理解state.go
+            // (function () {
+            //     $state.go(pageState);
+            // }());
 
-                });
-                for (var i = 0; i < vm.cards.length; i++) {
-                    if (parseInt(vm.cardNumber) === vm.cards[i].number) {
-                        toaster.pop('success', '', '您的一卡通已被系统回收');
-                        vm.cardIsReclaimed = true;
-                        break;
-                    }
-                }
-
-                if (vm.cardIsReclaimed === false) {
-                    toaster.pop('error', '', '您的一卡通未被系统回收');
-                }
+            switch (pageState) {
+                case 'blog': $state.go(pageState);
+                    break;
+                case '博客': $state.go('blog');
+                    break;
+                default:
+                    $state.go('home');
+                    //此处可以加toaster，但是还没调好
+                    //造轮子？
+                    break;
             }
-            vm.cardNumber = null;
-            vm.cardIsReclaimed = false;
         }
     }
 })();
